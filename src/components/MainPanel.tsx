21efc127703f9ea035572a01878c74960cbeddad
Soham Waghmare <102748742+sohamw03@users.noreply.github.com>
2024-10-09 01:47:59 +0530

    feat: home page UI 10% done; NEXTUI Sucks

--- src/components/MainPanel.tsx
+++ src/components/MainPanel.tsx
@@ -1,34 +1,56 @@
 "use client";
 
 import { useGlobal } from "@/drivers/GlobalContext";
-import { useEffect, useState } from "react";
+import { Button } from "@nextui-org/button";
+import { Card, CardFooter } from "@nextui-org/card";
+import { Image } from "@nextui-org/image";
+import { useEffect } from "react";
 
 export default function MainPanel(props: any) {
   // Global States
   const { listFiles, createFile, removeFile, user, files } = useGlobal();
 
-  const fetchFiles = async () => { listFiles(); };
+  const fetchFiles = async () => {
+    listFiles();
+  };
   useEffect(() => {
-    if (user.loggedIn === true) { fetchFiles(); }
+    if (user.loggedIn === true) {
+      fetchFiles();
+    }
   }, [user]);
   return (
+    // <div>
+    //   {props.page} Main
+    //   <button onClick={() => { createFile(); }}>
+    //     CreateFile
+    //   </button>
+    //   <div>
+    //     {files.map((file) => (
+    //       <div key={file.id} className="border m-2 p-2 flex flex-col gap-1 w-[8rem]">
+    //         <span>{file.name}</span>
+    //         <button
+    //           onClick={() => {
+    //             removeFile(file.id);
+    //           }}>
+    //           Remove
+    //         </button>
+    //       </div>
+    //     ))}
+    //   </div>
+    // </div>
     <div>
-      {props.page} Main
-      <button onClick={() => { createFile(); }}>
-        CreateFile
-      </button>
-      <div>
-        {files.map((file) => (
-          <div key={file.id} className="border m-2 p-2 flex flex-col gap-1 w-[8rem]">
-            <span>{file.name}</span>
-            <button
-              onClick={() => {
-                removeFile(file.id);
-              }}>
-              Remove
-            </button>
-          </div>
-        ))}
+      <h1 className="text-large font-semibold">Recents</h1>
+      <h2 className="text-medium font-medium">Photos</h2>
+      <div className="relative overflow-y-hidden flex items-center border border-default-200 dark:border-default-100 px-2 py-4 rounded-large overflow-hidden">
+        <Card radius="lg" className="border-none">
+          <Image alt="Woman listing to music" className="object-cover" height={200} src="https://nextui.org/images/hero-card.jpeg" width={200} />
+          <CardFooter className="">
+            <p className="text-tiny text-white/80">Available soon.</p>
+            <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg" size="sm">
+              Notify me
+            </Button>
+          </CardFooter>
+        </Card>
       </div>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,10 +2,61 @@ import { nextui } from "@nextui-org/theme";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}", "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
+  content: ["./components/**/*.{js,ts,jsx,tsx,mdx}", "./pages/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}", "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   darkMode: "class",
-  plugins: [nextui()],
+  plugins: [
+    nextui({
+      layout: {
+        dividerWeight: "1px",
+        disabledOpacity: 0.5,
+        fontSize: {
+          tiny: "0.75rem",
+          small: "0.875rem",
+          medium: "1.25rem",
+          large: "1.5rem",
+        },
+        lineHeight: {
+          tiny: "1rem",
+          small: "1.25rem",
+          medium: "1.5rem",
+          large: "1.75rem",
+        },
+        radius: {
+          small: "8px",
+          medium: "12px",
+          large: "0.5rem",
+        },
+        borderWidth: {
+          small: "1px",
+          medium: "2px",
+          large: "3px",
+        },
+      },
+      themes: {
+        light: {
+          layout: {
+            hoverOpacity: 0.8,
+            boxShadow: {
+              small: "0px 0px 5px 0px rgb(0 0 0 / 0.02), 0px 2px 10px 0px rgb(0 0 0 / 0.06), 0px 0px 1px 0px rgb(0 0 0 / 0.3)",
+              medium: "0px 0px 15px 0px rgb(0 0 0 / 0.03), 0px 2px 30px 0px rgb(0 0 0 / 0.08), 0px 0px 1px 0px rgb(0 0 0 / 0.3)",
+              large: "0px 0px 30px 0px rgb(0 0 0 / 0.04), 0px 30px 60px 0px rgb(0 0 0 / 0.12), 0px 0px 1px 0px rgb(0 0 0 / 0.3)",
+            },
+          },
+        },
+        dark: {
+          layout: {
+            hoverOpacity: 0.9,
+            boxShadow: {
+              small: "0px 0px 5px 0px rgb(0 0 0 / 0.05), 0px 2px 10px 0px rgb(0 0 0 / 0.2), inset 0px 0px 1px 0px rgb(255 255 255 / 0.15)",
+              medium: "0px 0px 15px 0px rgb(0 0 0 / 0.06), 0px 2px 30px 0px rgb(0 0 0 / 0.22), inset 0px 0px 1px 0px rgb(255 255 255 / 0.15)",
+              large: "0px 0px 30px 0px rgb(0 0 0 / 0.07), 0px 30px 60px 0px rgb(0 0 0 / 0.26), inset 0px 0px 1px 0px rgb(255 255 255 / 0.15)",
+            },
+          },
+        },
+      },
+    }),
+  ],
 };
