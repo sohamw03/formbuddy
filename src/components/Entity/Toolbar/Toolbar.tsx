8652da62dbb8ee8a3c664b8ec8d0276221b89642
sohamw03 <102748742+sohamw03@users.noreply.github.com>
2024-11-01 23:24:15 +0530

    chore: toolbar granular dom tree increment

--- src/components/Entity/Toolbar/Toolbar.tsx
+++ src/components/Entity/Toolbar/Toolbar.tsx
@@ -2,7 +2,11 @@ import styles from "./Toolbar.module.css";
 export default function Toolbar() {
   return (
     <div className={styles.toolbar}>
-      <div>Sizes 1920 x 1080 Crop</div>
+      <div>
+        <div>Sizes</div>
+        <div>1920 x 1080</div>
+        <div>Crop</div>
+      </div>
     </div>
   );
 }
