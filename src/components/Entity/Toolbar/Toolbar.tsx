58f850a4753033e353af16bd105ff574f478ddc9
Soham Waghmare <102748742+sohamw03@users.noreply.github.com>
2024-11-02 20:43:13 +0530

    feat: found libraries for cropping and resizing

--- src/components/Entity/Toolbar/Toolbar.tsx
+++ src/components/Entity/Toolbar/Toolbar.tsx
@@ -2,10 +2,14 @@ import styles from "./Toolbar.module.css";
 export default function Toolbar() {
   return (
     <div className={styles.toolbar}>
-      <div>
-        <div>Sizes</div>
+      <div className={styles.toolbarInWrapper}>
+        <div>100%
+          {/* https://imagekit.io/blog/image-cropping-in-react-application */}
+        </div>
         <div>1920 x 1080</div>
-        <div>Crop</div>
+        <div>Crop
+          {/* https://valentinh.github.io/react-easy-crop */}
+        </div>
       </div>
     </div>
   );
